49092461ee26842e8f840fd4a82b5c76c3cd728f
Rodrigo Pedrosa <rodris@gmail.com>
2020-01-19 19:17:29 -0300

    Setup for hosting at gh-pages.

---
+++ build/precache-manifest.50864543660ccad2e622b49853c76111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac52be0272ed72b8d9cdcddad9d7cc91",
+    "url": "/hex3/index.html"
+  },
+  {
+    "revision": "228f675bdeaf4ad6fecc",
+    "url": "/hex3/static/css/main.46d31ae9.chunk.css"
+  },
+  {
+    "revision": "28f2c383ba37db89cb06",
+    "url": "/hex3/static/js/2.4165c30f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hex3/static/js/2.4165c30f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "228f675bdeaf4ad6fecc",
+    "url": "/hex3/static/js/main.4a60f316.chunk.js"
+  },
+  {
+    "revision": "126d2414831a962ad96e",
+    "url": "/hex3/static/js/runtime-main.2a6d6a45.js"
+  },
+  {
+    "revision": "310b02e220437600912dccb9914e6dc7",
+    "url": "/hex3/static/media/board.310b02e2.svg"
+  }
+]);
